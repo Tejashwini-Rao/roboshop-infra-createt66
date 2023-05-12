63011b45d98fe6f2d01372c64fc0ca7964a93e93
Tejashwini Rao <polepalle.07@gmail.com>
2023-05-12 19:07:27 +0530

    Pushing all the components

---
+++ create.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##### Change these values ###
+ZONE_ID="Z005396725ZQYS9AQ6CZX"
+SG_NAME="new-allow"
+ENV="dev"
+#############################
+
+
+COMPONENT=all
+create_ec2() {
+  PRIVATE_IP=$(aws ec2 run-instances \
+      --image-id ${AMI_ID} \
+      --instance-type t3.micro \
+      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=Yes}]"  \
+      --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+      --security-group-ids ${SGID} \
+      --iam-instance-profile Name=SecretManager_Role_for_RoboShop_Nodes \
+      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+}
+
+#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=ami-0a8ac5a5f29e1167c
+SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
+
+
+for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
+  COMPONENT="${component}-${ENV}"
+  create_ec2
+done
+
+
